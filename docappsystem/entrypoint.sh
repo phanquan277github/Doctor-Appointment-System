9797d264fcdbad4dd8199260c5efed34f4147b86
Nguyen Trung Nguyen <nguyenkun555@gmail.com>
2026-03-31 22:38:32 +0700

    Test update RDS & S3

--- docappsystem/entrypoint.sh
+++ docappsystem/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-# Dừng script nếu có bất kỳ lệnh nào bị lỗi trong quá trình thực thi
+# Dừng script nếu có bất kỳ lệnh nào bị lỗi
 set -e
 
-# Hệ thống sử dụng AWS RDS nên luôn ở trạng thái Available, tiến hành Migrate trực tiếp
 echo "AWS RDS is ready! Applying Database Migrations..."
 
-# TỰ ĐỘNG ĐỒNG BỘ CẤU TRÚC DATABASE
+# --- 2 LỆNH TỰ ĐỘNG ĐỒNG BỘ CẤU TRÚC DATABASE ---
 python manage.py makemigrations
 python manage.py migrate
 
 echo "Migrations completed! Starting Django Server..."
 
-# Chạy lệnh khởi động máy chủ Web thực sự (gunicorn...) được truyền từ docker-compose
+# Chạy lệnh thực sự khởi tạo Gunicorn
 exec "$@"
