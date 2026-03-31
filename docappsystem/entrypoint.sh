5abccb916ef2f69ea922522aa7c8e0f5f7f4c308
Nguyen Trung Nguyen <nguyenkun555@gmail.com>
2026-03-31 22:39:22 +0700

    Fix: Loai bo vong lap cho container db cuc bo trong entrypoint

--- docappsystem/entrypoint.sh
+++ docappsystem/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-# Dừng script nếu có bất kỳ lệnh nào bị lỗi
+# Dừng script nếu có bất kỳ lệnh nào bị lỗi trong quá trình thực thi
 set -e
 
+# Hệ thống sử dụng AWS RDS nên luôn ở trạng thái Available, tiến hành Migrate trực tiếp
 echo "AWS RDS is ready! Applying Database Migrations..."
 
-# --- 2 LỆNH TỰ ĐỘNG ĐỒNG BỘ CẤU TRÚC DATABASE ---
+# TỰ ĐỘNG ĐỒNG BỘ CẤU TRÚC DATABASE
 python manage.py makemigrations
 python manage.py migrate
 
 echo "Migrations completed! Starting Django Server..."
 
-# Chạy lệnh thực sự khởi tạo Gunicorn
+# Chạy lệnh khởi động máy chủ Web thực sự (gunicorn...) được truyền từ docker-compose
 exec "$@"
